da35276bf0d6067e222446c5956b7086f57907e1
Hasmirre <lagman.jrgreggvictor@gmail.com>
2022-11-27 00:10:23 +0800

    made progress with interfaces

---
+++ WebApplication1/web/logic.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $("#header").load("./components/header.html");
+    $("#header-auth").load("./components/header-auth.html");
+    $("#footer").load("./components/footer.html");
+
+    $('[data-quantity="plus"]').click(function (e) {
+        e.preventDefault();
+        fieldName = $(this).attr("data-field");
+        var currentVal = parseInt($("input[name=" + fieldName + "]").val());
+        if (!isNaN(currentVal)) {
+            $("input[name=" + fieldName + "]").val(currentVal + 1);
+        } else {
+            $("input[name=" + fieldName + "]").val(0);
+        }
+    });
+    
+    $('[data-quantity="minus"]').click(function (e) {
+        e.preventDefault();
+        fieldName = $(this).attr("data-field");
+        var currentVal = parseInt($("input[name=" + fieldName + "]").val());
+        if (!isNaN(currentVal) && currentVal > 1) {
+            $("input[name=" + fieldName + "]").val(currentVal - 1);
+        } else {
+            $("input[name=" + fieldName + "]").val(1);
+        }
+    });
+});
